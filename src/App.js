ab8abc68530dced086fb2833e4e11f57cfc890ef
Faiz Ali <faizplus@gmail.com>
2021-05-29 05:56:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import GameBoard from "./screens/GameBoard";
+import GameStart from "./screens/GameStart";
+import Loader from "./components/Loader";
+import API from "./services/api";
 
 function App() {
+  const [gameData, setGameData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const getGameData = async () => {
+    if (localStorage.getItem("file_id")) {
+      const { data } = await API.post("api/get-game-data", {
+        fileId: localStorage.getItem("file_id"),
+      });
+
+      setGameData(data);
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  };
+
+  const difficultySelected = async (selectedDifficulty) => {
+    setLoading(true);
+    const { data } = await API.post("api/start-game", {
+      difficulty: selectedDifficulty,
+    });
+
+    localStorage.setItem("file_id", data.file_id);
+
+    setGameData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getGameData();
+  }, []);
+
+  const handleCardUpdate = (cards) => {
+    return new Promise(async (resolve, reject) => {
+      const newGameData = { ...gameData, cards: cards };
+
+      const { data } = await API.post("api/select-card", {
+        gameData: newGameData,
+      });
+
+      setGameData(data);
+      resolve(data);
+    });
+  };
+
+  const handleRestart = () => {
+    localStorage.removeItem("file_id");
+    setGameData({});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {gameData.file_id ? (
+            <GameBoard
+              gameData={gameData}
+              onCardUpdate={handleCardUpdate}
+              onRestart={handleRestart}
+            />
+          ) : (
+            <GameStart onDifficultySelection={difficultySelected} />
+          )}
+        </>
+      )}
+    </>
   );
 }
 
